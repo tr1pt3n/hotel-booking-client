7b401ebf395952ab14dec090557870666c269595
kiethongngu <21522263@gm.uit.edu.vn>
2023-11-22 15:41:03 +0700

    UI: Sign in/ Sign up

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Home from "./pages/home/Home"
+
+import Authentication from "./pages/authentication/authentication";
+import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/hotels" element={<List />}></Route>
-        <Route path="/hotels/:id" element={<Hotel />}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}></Route>
+    //     <Route path="/hotels" element={<List />}></Route>
+    //     <Route path="/hotels/:id" element={<Hotel />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <Authentication/>
   );
 }
 
