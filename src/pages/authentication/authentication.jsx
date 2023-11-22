2d01488399a4d9680a82e8806b34942233eff74c
kiethongngu <21522263@gm.uit.edu.vn>
2023-11-22 17:05:28 +0700

    Fix swap button

--- src/pages/authentication/authentication.jsx
+++ src/pages/authentication/authentication.jsx
@@ -1,87 +1,101 @@
+import React, { useState, useRef } from 'react';
 import './authentication.css';
-const Authentication = () =>
-{
 
-    return (
+const Authentication = () => {
+  const containerRef = useRef(null);
 
-<>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-  />
-  <div className="container" id="container">
-    <div className="form-container sign-up">
-      <form>
-        <h1>Create Account</h1>
-        <div className="social-icons">
-          <a href="#" className="icon">
-            <i className="fa-brands fa-google-plus-g" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-facebook-f" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-github" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-linkedin-in" />
-          </a>
-        </div>
-        <span>or use your email for registeration</span>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+  const registerBtnOnClick = () => {
+    if (containerRef.current) {
+      containerRef.current.classList.add('active');
+    }
+  };
 
-        <button>Sign Up</button>
-      </form>
-    </div>
-    <div className="form-container sign-in">
-      <form>
-        <h1>Sign In</h1>
-        <div className="social-icons">
-          <a href="#" className="icon">
-            <i className="fa-brands fa-google-plus-g" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-facebook-f" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-github" />
-          </a>
-          <a href="#" className="icon">
-            <i className="fa-brands fa-linkedin-in" />
-          </a>
+  const loginBtnOnClick = () => {
+    if (containerRef.current) {
+      containerRef.current.classList.remove('active');
+    }
+  };
+
+
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      />
+      <div className="container" ref={containerRef}>
+        <div className="form-container sign-up">
+          <form>
+            <h1>Create Account</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in" />
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+
+            <button>Sign Up</button>
+          </form>
         </div>
-        <span>or use your email password</span>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <a href="#">Forget Your Password?</a>
-        <button>Sign In</button>
-      </form>
-    </div>
-    <div className="toggle-container">
-      <div className="toggle">
-        <div className="toggle-panel toggle-left">
-          <h1>Welcome Back!</h1>
-          <p>Enter your personal details to use all of site features</p>
-          <button className="hidden" id="login">
-            Sign In
-          </button>
+        <div className="form-container sign-in">
+          <form>
+            <h1>Sign In</h1>
+            <div className="social-icons">
+              <a href="#" className="icon">
+                <i className="fa-brands fa-google-plus-g" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-facebook-f" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-github" />
+              </a>
+              <a href="#" className="icon">
+                <i className="fa-brands fa-linkedin-in" />
+              </a>
+            </div>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forget Your Password?</a>
+            <button>Sign In</button>
+          </form>
         </div>
-        <div className="toggle-panel toggle-right">
-          <h1>Hello, Friend!</h1>
-          <p>Register with your personal details to use all of site features</p>
-          <button className="hidden" id="register">
-            Sign Up
-          </button>
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button className="hidden" id="login" onClick={loginBtnOnClick}>
+                Sign In
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello, Friend!</h1>
+              <p>Register with your personal details to use all of site features</p>
+              <button className="hidden" id="register" onClick={registerBtnOnClick}>
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</>
-)
-}
+    </>
+  );
+};
 
-export default Authentication
+export default Authentication;
